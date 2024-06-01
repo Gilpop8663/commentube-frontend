466d229dc4385aea839640375706d7637f036eb3
gilpop8663 <wolfye@naver.com>
2024-06-01 20:44:40 +0900

    chore: tailwind, graphQL 환경설정

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import { gql, useQuery } from "@apollo/client";
+
+const GET_LOCATIONS = gql`
+  query {
+    getAllVideos {
+      id
+      videoUrl
+      createdAt
+      updatedAt
+      likes
+      dislikes
+      comments {
+        id
+        content
+        nickname
+      }
+    }
+  }
+`;
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  const { loading, error, data } = useQuery(GET_LOCATIONS);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error : {error.message}</p>;
+
+  console.log(data);
+
+  return <div>안녕</div>;
+}
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,18 @@
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+
+const client = new ApolloClient({
+  uri: "http://localhost:3000/graphql",
+  cache: new InMemoryCache(),
+});
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <ApolloProvider client={client}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ApolloProvider>
+);
